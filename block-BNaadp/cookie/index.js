6323521128d8726803bfc72a80e52ce1e4ae1a90
Piyush Chamoli <piyushchamoli11@gmail.com>
2022-03-11 19:10:19 +0530

    update index.js

--- block-BNaadp/cookie/index.js
+++ block-BNaadp/cookie/index.js
@@ -3,8 +3,19 @@ var cookieParser = require('cookie-parser')
 
 var app = express()
 
+app.use(cookieParser())
+
+app.use((req,res,next) => {
+    res.cookie('name', 'piyush')
+    next()
+})
+
 app.get('/', (req,res) => {
     res.send('Home Page')
+})
+
+app.get('/cookies', (req,res) => {
+    res.json(req.cookies)
 })
 
 app.listen(3000, () => {
